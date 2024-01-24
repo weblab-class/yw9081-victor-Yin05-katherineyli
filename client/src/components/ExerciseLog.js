2d56ea983824e9c68f55736a54870ce5116a6771
Katherine Li <math4fun2015@gmail.com>
2024-01-24 14:55:12 -0500

    styling exercises and nutrition logs

--- client/src/components/ExerciseLog.js
+++ client/src/components/ExerciseLog.js
@@ -7,7 +7,7 @@ const ExerciseLog = (props) => {
   return (
     <div className="w-3/4 flex-col">
       <div className="font-semibold text-lg py-2">Exercise Log</div>
-      <div className="w-full h-96 overflow-auto">
+      <div className="w-full h-72 overflow-auto">
         {props.exercises.map((exercise) => (
           <ExerciseItem type={exercise.type} duration={exercise.duration} date={exercise.date} />
         ))}
