54f03e744f9ab8b7aa403c6b66d7f548f37c269b
Victoryin05 <66051125+Victoryin05@users.noreply.github.com>
2024-01-25 20:49:46 -0500

    fixedPut

--- server/api.js
+++ server/api.js
@@ -50,22 +50,23 @@ router.get("/scores", (req, res) => {
   IdScore.find({ creator_id: req.query.id }).then((anIdScore) => res.send(anIdScore));
 });
 
-router.put("/scores", (req, res) => {
-  IdScore.findOne({ creator_id: req.query.id }).then((anIdScore) => {
-    anIdScore.scores = req.body.newScores;
-    anIdScore.save().then((newIdScore) => {
-      res.send(newIdScore);
-    });
-  });
-});
-
 router.post("/scores", (req, res) => {
-  console.log("THE USER ID RIGHT NOW IS " + req.body.id);
-  const newIdScore = new IdScore({
-    creator_id: req.body.id,
-    scores: { core: 0, arms: 0, legs: 0, cardio: 0 },
-  });
-  newIdScore.save().then((anIdScore) => res.send(anIdScore));
+  if (req.body.theRequest == 0) {
+    console.log("VICTOR IS IN");
+    const newIdScore = new IdScore({
+      creator_id: req.body.id,
+      scores: { core: 0, arms: 0, legs: 0, cardio: 0 },
+    });
+    newIdScore.save().then((anIdScore) => res.send(anIdScore));
+  } else {
+    console.log("VICTOR IS UPDATING THINGS");
+    IdScore.findOne({ creator_id: req.body.id }).then((anIdScore) => {
+      anIdScore.scores = req.body.newScores;
+      anIdScore.save().then((newIdScore) => {
+        res.send(newIdScore);
+      });
+    });
+  }
 });
 
 router.get("/exercises", (req, res) => {
